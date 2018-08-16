96a008262cd136dafad686bf6bfb62d0171791f8
danielGonzaloCapelli <34524606+danielGonzaloCapelli@users.noreply.github.com>
2018-08-16 12:07:29 -0300

    Initial commit

---
+++ Html5/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Html5.Startup))]
+namespace Html5
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
